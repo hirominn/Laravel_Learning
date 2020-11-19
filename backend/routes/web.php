f5e02128dad3201c7a9e84281f29f109a536c086
17t2037h <17t2037h@shinshu-u.ac.jp>
2020-11-20 03:12:52 +0900

    made a controller HelloController and added an action.

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -16,3 +16,42 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('hello', function(){
+//     return '<html><body>
+//     <h1>Hello</h1><p>
+//     This is sample page.
+//     </p>
+//     </body>
+//     </html>';
+// });
+
+
+// Route::get('hello/{msg?}', function($msg='no message.') { //msgに?をつけて任意パラメータとする
+//     $html = <<<EOF
+//     <html>
+//     <head>
+//     <title>Hello!</title>
+//     <style>
+//     body {font-size:16pt; color:#999;}
+//     h1{font-size:100px; text-align:right; color:#eee;
+//         margin:-40px 0px -50px 0px;}
+//     </style>
+//     </head>
+//     <body>
+//         <h1>Hello</h1>
+//         <p>
+//         {$msg}
+//         </p>
+//         <p>
+//         これは、サンプルで作ったページです。
+//         </p>
+//     </body>
+//     </html>
+//     EOF;
+//
+//     return $html;
+// });
+
+use App\Http\Controllers\HelloController;
+Route::get('hello', [HelloController::class, 'Index']); //記法::https://qiita.com/norichintnk/items/34a04cd17bfe4014313a
