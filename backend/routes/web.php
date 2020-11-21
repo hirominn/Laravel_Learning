4ccd47e41844ab6107fa0eeb304fd22f6d2f9f9e
17t2037h <17t2037h@shinshu-u.ac.jp>
2020-11-22 01:23:06 +0900

    use multiple actions. p.47

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -54,4 +54,6 @@ Route::get('/', function () {
 // });
 
 use App\Http\Controllers\HelloController;
-Route::get('hello/{id?}/{pass?}', [HelloController::class, 'Index']); //記法::https://qiita.com/norichintnk/items/34a04cd17bfe4014313a
+Route::get('hello', [HelloController::class, 'Index']); //記法::https://qiita.com/norichintnk/items/34a04cd17bfe4014313a
+
+Route::get('hello/other', [HelloController::class, 'other']);
