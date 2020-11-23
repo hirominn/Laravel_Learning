770704e3ed78c5cfdb1d6538d355016f9670b0d7
17t2037h <17t2037h@shinshu-u.ac.jp>
2020-11-24 01:16:38 +0900

    creating a layout. p85

--- backend/app/Http/Controllers/HelloController.php
+++ backend/app/Http/Controllers/HelloController.php
@@ -8,7 +8,11 @@ use Illuminate\Http\Response;
 class HelloController extends Controller
 {
     public function index(){
-        $data = ['one', 'two', 'three', 'four', 'five'];
+        $data = [
+            ['name'=>'山田太郎', 'mail'=>'taro@yamada'],
+            ['name'=>'田中花子', 'mail'=>'hanako@flower'],
+            ['name'=>'鈴木さちこ', 'mail'=>'sachico@happy']
+        ];
         return view('hello.index', ['data'=>$data]); //viewは、コンテンツにテンプレート(hello/index.blade.php)のレンダリング結果を設定したResponseインスタンスを返す
     }
 
--- backend/resources/views/hello/index.blade.php
+++ backend/resources/views/hello/index.blade.php
@@ -1,29 +1,23 @@
-<html>
-<head>
-    <title>Hello/Index</title>
-    <style>
-    body {font-size:16pt; color:#999;}
-    h1 {font-size:50pt; text-align: right; color:#f6f6f6;
-        margin:-20px 0px -30px 0px; letter-spacing: -4pt;}
-    </style>
-</head>
-<body>
-    <h1>Blade/Index</h1>
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+    @parent
+        インデックスページ
+@endsection
+
+@section('content')
     <p>
-        &#064;forディレクティブの例
+        ここが本文のコンテンツです。
     </p>
-    <ol>
-        @php
-            $counter = 0;
-        @endphp
-        @while($counter < count($data))
-            <li>
-                {{$data[$counter]}}
-            </li>
-            @php
-                $counter++;
-            @endphp
-        @endwhile
-    </ol>
-</body>
-</html>
+    <p>
+        必要なだけ記述できます。
+    </p>
+    @include('components.message', ['msg_title'=>'OK', 'msg_content'=>'サブビューです。'])
+    @each('components.item', $data, 'item')
+@endsection
+
+@section('footer')
+    copyright 2017 tuyano.
+@endsection
