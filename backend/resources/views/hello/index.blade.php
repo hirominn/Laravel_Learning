7eb3a7b36302cd2654fa727ab4778671ff1c2fb9
17t2037h <17t2037h@shinshu-u.ac.jp>
2021-02-15 02:08:11 +0900

    checked about CSRF, COOKIE and REDIRECT. p154

--- backend/resources/views/hello/index.blade.php
+++ backend/resources/views/hello/index.blade.php
@@ -15,20 +15,37 @@
     <p>
         入力に問題があります。再入力してください。
     </p>
-    <!-- <div>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>
-                    {{ $error}}
-                </li>
-            @endforeach
-        </ul>
-    </div> -->
     @endif
     <table>
         <form action="/hello" method="post">
             {{ csrf_field() }}
-            @if ($errors->has('name'))
+            @if ($errors->has('msg'))
+            <tr>
+                <th>
+                    ERROR
+                </th>
+                <td>
+                    {{$errors->first('msg')}}
+                </td>
+            </tr>
+            @endif
+            <tr>
+                <th>
+                    Message:
+                </th>
+                <td>
+                    <input type="text" name="msg" value="{{old('msg')}}" />
+                </td>
+            </tr>
+            <tr>
+                <th>
+
+                </th>
+                <td>
+                    <input type="submit" value="send" />
+                </td>
+            </tr>
+            <!-- @if ($errors->has('name'))
             <tr>
                 <th>
                     ERROR
@@ -81,7 +98,7 @@
                     value:
                 </th>
                 <td><input type="submit" name="send" /></td>
-            </tr>
+            </tr> -->
 
         </form>
     </table>
